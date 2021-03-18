8c2df0a8baecde6838fa9bee6d5f3e73a5bb0392
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-03-18 14:45:01 -0500

    Added sprites, modified main menu

--- Mappy/Assets/Code/MainMenu.cs
+++ Mappy/Assets/Code/MainMenu.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    //Hewwo?
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
